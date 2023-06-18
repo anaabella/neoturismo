4c576e54d98ecb89729308af309478787115355a
anabellamedrano <133698052+anabellamedrano@users.noreply.github.com>
2023-06-18 19:50:08 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function openImage(imageUrl) {
-    document.getElementById("modal-image").src = imageUrl;
-    document.getElementById("image-modal").style.display = "block";
-  }
-  
-  function closeImage() {
-    document.getElementById("image-modal").style.display = "none";
-  }
